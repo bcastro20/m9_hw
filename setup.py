a0fe154561c3152e527ea79184ae1fa93f6105a3
bcastro20 <64329864+bcastro20@users.noreply.github.com>
2024-07-03 19:47:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+from setuptools import setup
 
+setup(
+    name = 'm9hw',
+    version = '0.1',
+    description = 'hw',
+    url = 'https://github.com/bcastro20/m9hw.git',
+    author = 'bcastro',
+    license = 'MIT',
+    pacakges = ['mypackage']
+)
